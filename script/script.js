7dd94afc6f9c912ec201c7577b73f871eb126577
andresmartinezpineda <andrespineda_0@outlook.com>
2024-05-02 12:32:22 -0500

    primer commit de audio andres

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let elemento = document.getElementsByClassName('list')
+
+console.log(elemento)
+
+let hermanos = document.querySelector('ul.listas-cursos > li.list')
+
+console.log(hermanos.nextElementSibling.nextElementSibling)
