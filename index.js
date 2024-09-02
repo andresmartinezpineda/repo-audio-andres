d7a1d2e3bad7d8681f1fa5440973325edb8cb390
andresmartinezpineda <andrespineda_0@outlook.com>
2024-09-02 11:24:36 -0500

    cambios del dia 02-09-2024

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function añadir_carrito(parametro){
     
     document.getElementById(parametro).style.display='block';
 }
+
+function quitar_carrito(parametro){
+    
+    document.getElementById(parametro).style.display='none';
+}
